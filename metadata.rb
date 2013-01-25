f196e704c4771ecd38065cf0f4ce6ed8ed28ec0a
Nick Muerdter <nick.muerdter@nrel.gov>
2013-01-25 16:48:22 -0700

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "nick.muerdter@nrel.gov"
 license          "All rights reserved"
 description      "Installs/Configures rsyslog"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
